4abe25774b21fbb823a4fe176ca01ad55c98010b
Beren Millidge <bmillidgework@gmail.com>
2017-04-06 22:12:33 +0100

    initial commit for log reg
    add to this and finish off, ideally tomorrow, if you get up early and do work.

---
+++ logistic-regression.py
@@ -0,0 +1,7 @@
+# okay, here is where I do my logistic regression. Which should be fairly simple to implement once we have linear regression working!
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+#okay, fill the rest in later - this is just to get the github commit
